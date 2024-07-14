f5c7bcbc29a9abf0e18f3e839885b87ceaa693f0
Aviral Tripathi <aviralotrip@gmail.com>
2024-07-14 22:40:30 +0530

    updated codes and output

--- backend/codes/a2b09dc3-3543-4bc7-bde4-e8cb99a7f1c5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <bits/stdc++.h>
-int main(){
-for(let i = 0;i<6;i++){
-std::cout<<"Hello World!!"<<std::endl;
-}
-;
-return 0;
-}
--- backend/codes/b883c0be-0a58-4f5d-ae3f-7e450e7137d8.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <bits/stdc++.h>
-int main(){
-printf("hello");
-return 0;
-}
